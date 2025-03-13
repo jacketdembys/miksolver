af5412bb4335b028d042fe09ce4033e30c1ebd88
jacketdembys <jacketdembys@yahoo.fr>
2025-03-13 18:34:25 -0500

    extending the lbe idea

--- create_multi_experiments.py
+++ create_multi_experiments.py
@@ -135,7 +135,7 @@ config_info = {
             'RESUMED_G_MODEL': "",
         },
         'HYPERPARAMETERS': {
-            'EPOCHS': 50,
+            'EPOCHS': 1000,
             'BATCH_SIZE': 128, #128, #100000
             'SHUFFLE': True,
             'NUM_WORKERS': 4,
