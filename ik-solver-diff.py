1435c6199160e9d350f5b0d520a4984a5460b65b
jacketdembys <jacketdembys@yahoo.fr>
2025-04-14 12:43:45 -0500

    new ideas

--- ik-solver-diff.py
+++ ik-solver-diff.py
@@ -11,8 +11,8 @@ class DiffIKDataset(Dataset):
         self.q = torch.from_numpy(np.load(filename_Q)).float() #.to(device)
         self.pose = torch.from_numpy(np.load(filename_D)).float() #.to(device)
         
-        print(f"Loaded Dataset\nq.shape: {self.q.shape}\npose.shape: {self.pose.shape}")
-        print(f"Device: {self.q.device}")
+        print(f"Loaded Dataset:\nq.shape: {self.q.shape}\npose.shape: {self.pose.shape}")
+        print(f"Loaded Dataset initially on --> Device: {self.q.device}")
 
         assert self.q.shape[0] == self.pose.shape[0], "Mismatch in sample count"
 
